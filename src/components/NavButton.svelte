375b72fc46810c0dd1ebb4bb2d04eac4a83c29dd
fjallnari <draconirx@gmail.com>
2023-09-11 18:54:20 +0200

    Init svelte, add simple header

---
+++ src/components/NavButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { currentNav } from "../../stores";
+
+    export let title: string;
+</script>
+
+<button class="{$currentNav === title ? "text-oxford-300 border-b-white" : ""} transition-colors" on:click={() => currentNav.set(title)}>
+    {title}
+</button>
---
+++ src/data/projects.ts
@@ -0,0 +1,9 @@
+const PROJECTS = [
+    {
+        id: "morii-vtt",
+        title: "Morii VTT",
+        body: "A virtual tabletop for playing tabletop RPGs online.",
+    }
+]
+
+export default PROJECTS;
