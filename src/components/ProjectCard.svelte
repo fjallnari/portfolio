600c720c9997a20a533b74e2305b6b8e8097ddb5
fjallnari <draconirx@gmail.com>
2023-09-13 18:54:00 +0200

    Add astro routing

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -2,20 +2,20 @@
     import TAGS from "../data/tags";
     import type ProjectInterface from "../interfaces/ProjectInterface";
     import Icon from '@iconify/svelte';
+    import TimelineDelim from "./TimelineDelim.svelte";
 
     export let project: ProjectInterface;
 
 </script>
 
-<li class="grid grid-cols-5 grid-rows-3 gap-2 p-2 cursor-pointer rounded active:bg-oxford-700 transition-colors">
+<a href="/projects/{project.id}" class="grid grid-cols-5 grid-rows-3 gap-2 p-2 cursor-pointer rounded active:bg-oxford-700 transition-colors">
     <div class="row-span-4 col-span-1 flex flex-row justify-around items-center">
         <div class="flex flex-col justify-center items-center text-xl text-timberwolf">
             <p>{project.to ?? '-'}</p>
             <div class="border-gray-500 border-solid border-b-2 w-16 h-1 m-1" />
             <p>{project.from ?? '-'}</p>
         </div>
-
-        <svg class="fill-timberwolf h-28 w-2" viewBox="0 0 23 284" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><path d="M22.34,0.894l0,111.598c0,0.493 -0.4,0.894 -0.893,0.894l-20.553,-0c-0.494,-0 -0.894,-0.401 -0.894,-0.894l0,-111.598c0,-0.494 0.4,-0.894 0.894,-0.894l20.553,-0c0.493,-0 0.893,0.4 0.893,0.894Zm0,170.078l0,111.599c0,0.493 -0.4,0.894 -0.893,0.894l-20.553,-0c-0.494,-0 -0.894,-0.401 -0.894,-0.894l0,-111.599c0,-0.493 0.4,-0.893 0.894,-0.893l20.553,-0c0.493,-0 0.893,0.4 0.893,0.893Zm-11.17,-40.41c6.165,0 11.17,5.005 11.17,11.17c0,6.165 -5.005,11.17 -11.17,11.17c-6.165,0 -11.17,-5.005 -11.17,-11.17c0,-6.165 5.005,-11.17 11.17,-11.17Z"/></svg>
+        <TimelineDelim />
     </div>
     <div class="col-span-5 text-xl">
         {project.title}
@@ -32,4 +32,4 @@
         {/each}
     </div>
 
-</li>
+</a>
---
+++ src/components/TimelineDelim.svelte
@@ -0,0 +1,8 @@
+
+<svg class="fill-gray-500 h-28 w-2" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 11 241">
+    <path d="M10.987.439 10.548 0H.439L0 .439v239.737l.439.439h10.109l.439-.439V.439Z" style="fill:#ebebeb"/>
+</svg>
+
+<!-- <svg class="fill-timberwolf h-28 w-2" viewBox="0 0 23 284" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <path d="M22.34,0.894l0,111.598c0,0.493 -0.4,0.894 -0.893,0.894l-20.553,-0c-0.494,-0 -0.894,-0.401 -0.894,-0.894l0,-111.598c0,-0.494 0.4,-0.894 0.894,-0.894l20.553,-0c0.493,-0 0.893,0.4 0.893,0.894Zm0,170.078l0,111.599c0,0.493 -0.4,0.894 -0.893,0.894l-20.553,-0c-0.494,-0 -0.894,-0.401 -0.894,-0.894l0,-111.599c0,-0.493 0.4,-0.893 0.894,-0.893l20.553,-0c0.493,-0 0.893,0.4 0.893,0.893Zm-11.17,-40.41c6.165,0 11.17,5.005 11.17,11.17c0,6.165 -5.005,11.17 -11.17,11.17c-6.165,0 -11.17,-5.005 -11.17,-11.17c0,-6.165 5.005,-11.17 11.17,-11.17Z"/>
+</svg> -->
