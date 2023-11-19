5207997c7052325137332042ef5b1698a7272345
fjallnari <draconirx@gmail.com>
2023-11-19 23:48:48 +0100

    Add redesigned mockup homepage

---
+++ src/components/new/NewProjectBento.svelte
@@ -0,0 +1,55 @@
+<script>
+    import NewProjectCard from "./NewProjectCard.svelte";
+
+</script>
+
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-6 pb-8">
+    <div class="grid gap-6">
+        <NewProjectCard 
+            name="morii"
+            type="web app"
+            colorBg="#3D2824" 
+            colorText="#E2E2E2"
+            imageSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" 
+        />
+        <NewProjectCard 
+            name="ttrpg compass"
+            type="website"
+            colorBg="#111A20"
+            colorText="#B69048"
+            imageSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" 
+        />
+
+        <NewProjectCard 
+            name="cartographer"
+            type="web app"
+            colorBg="#24363D"
+            colorText="#B69048"
+            imageSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" 
+        />
+    </div>
+    <div class="grid gap-6">
+        <NewProjectCard 
+            name="terramem"
+            type="branding"
+            colorBg="#202419" 
+            colorText="#B1B1A7"
+            imageSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" 
+        />
+        <NewProjectCard 
+            name="shadowtorch"
+            type="web & mobile app"
+            colorBg="#114470"
+            colorText="#7DD3FC"
+            imageSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" 
+        />
+        <NewProjectCard 
+            name="???"
+            type="poster design"
+            colorBg="#4F3C62" 
+            colorText="#E4E4E4"
+            imageSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" 
+        />
+    </div>
+</div>
---
+++ src/components/new/NewProjectCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let name: string;
+    export let type: string;
+    export let colorBg: string;
+    export let colorText: string;
+    export let imageSrc: string;
+
+</script>
+
+<div class="h-auto max-w-full rounded-md flex justify-center align-center relative font-eiko cursor-pointer" 
+    style="background-image: url({imageSrc});"
+>
+    <div class="absolute left-0 top-0 rounded w-full h-full hover:opacity-0 transition-opacity ease-in duration-150" style="background-color: {colorBg};"></div>
+    <div class="flex flex-col gap-2 text-4xl text-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] p-4 rounded" style="color: {colorText}; background-color: {colorBg};">
+        <h2>{name}</h2>
+        <h2 class="text-2xl text-gray-400">{type}</h2>
+    </div>
+    <img class="h-auto max-w-full rounded-md"
+        src="{imageSrc}" 
+        alt=""
+        style="visibility: hidden;"
+    >
+</div>
